46c6ecba7292d7fda81d9c92d8b9aad9c922e5a5
jmanrocks152 <jmanrocks152@gmail.com>
2026-02-11 20:01:51 -0600

    Updated image optimization

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     output: 'export',
-    basePath: '/~phungj'
+    basePath: '/~phungj',
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
