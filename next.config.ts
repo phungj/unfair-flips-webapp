dc07cbd0352f614aac92ed28b2fe2d9e0283c682
jmanrocks152 <jmanrocks152@gmail.com>
2026-02-21 23:39:47 -0600

    Updated base path

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     output: 'export',
-    basePath: '/~phungj',
+    basePath: '/~phungj/unfair-flips',
     images: {
         unoptimized: true,
     },
